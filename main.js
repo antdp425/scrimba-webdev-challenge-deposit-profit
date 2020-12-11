b666d2b4b8063dbff72c42980386b3d279337ddd
antdp425 <antdp425@aol.com>
2020-12-10 21:44:50 -0500

    solved

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 function depositProfit(deposit, rate, threshold) {
-   //  write code here.
+  let yearsToProfit = 0;
+  let total = deposit;
+  while(total < threshold){
+     total += total * (rate/100)
+     yearsToProfit++
+  }
+  return yearsToProfit;
 }
 
-
-
 /**
-* Test Suite 
-*/
-describe('depositProfit()', () => {
-   it('returns number of years it will take to hit threshold based off of deposit & rate', () => {
-       // arrange
-       const deposit = 100;
-       const rate = 20;
-       const threshold = 170;
-       
-       // act
-       const result = depositProfit(deposit, rate, threshold)
+ * Test Suite
+ */
+describe("depositProfit()", () => {
+  it("returns number of years it will take to hit threshold based off of deposit & rate", () => {
+    // arrange
+    const deposit = 100;
+    const rate = 20;
+    const threshold = 170;
 
-       // log
-       console.log("result: ", result);
-       
-       // assert
-       expect(result).toBe(3);
-   });
+    // act
+    const result = depositProfit(deposit, rate, threshold);
+
+    // log
+    console.log("result: ", result);
+
+    // assert
+    expect(result).toBe(3);
+  });
 });
